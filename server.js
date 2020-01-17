c23b2370aeb657c6fbe04aaaafd257d0108c247a
william-parker <edwinious@gmail.com>
2020-01-16 21:54:37 -0500

    update

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+/*
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
@@ -24,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+}*/
 
 const PORT = process.env.PORT || 5000;
 
